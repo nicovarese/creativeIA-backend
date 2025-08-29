cb826eb8c7a2bd89c6617dd70b2b20e82462457a
Natalia Vignola <natalia.vignola@hotmail.com>
2025-08-29 12:21:56 -0300

    Creación proyecto SpringBoot sin configs

---
+++ src/main/java/com/ryn/creativeai/CreativeaiApplication.java
@@ -0,0 +1,13 @@
+package com.ryn.creativeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativeaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativeaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryn/creativeai/CreativeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryn.creativeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativeaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
